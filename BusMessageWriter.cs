37e6f424d6d4b5db44a5fafd45dec0fbef1a612f
Daniel Krasnenko <63058931+Zilfatto@users.noreply.github.com>
2022-10-10 15:25:23 +0700

    Add the solution for the test task

---
+++ BusMessageWriter.cs
@@ -0,0 +1,41 @@
+namespace Multithreading.Utils;
+
+public class BusMessageWriter : IAsyncDisposable
+{
+    private readonly IBusConnection _connection;
+    
+    private readonly MemoryStream _buffer = new();
+    
+    private readonly Semaphore _pool = new(1, 1);
+
+    public BusMessageWriter(IBusConnection connection)
+    {
+        _connection = connection;
+    }
+    
+    public async Task SendMessageAsync(byte[] nextMessage)
+    {
+        _pool.WaitOne();
+        
+        _buffer.Write(nextMessage, 0, nextMessage.Length);
+
+        if (_buffer.Length > 100)
+        {
+            await _connection.PublishAsync(_buffer.ToArray());
+            _buffer.SetLength(0);
+        }
+
+        _pool.Release();
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        if (_buffer.Length > 0)
+        {
+            await _connection.PublishAsync(_buffer.ToArray());
+        }
+        
+        await _buffer.DisposeAsync();
+        GC.SuppressFinalize(this);
+    }
+}
